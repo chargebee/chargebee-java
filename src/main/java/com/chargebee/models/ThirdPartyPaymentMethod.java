496ba117fad8ce1c9d516e53b83b54b3d5b77742
SangeethaBaskaran <sangeetha@chargebee.com>
2016-06-27 15:56:19 +0530

    * * *
    ** APIs updated**:
    New resource "third_party_payment_method" is returned on performing copy card for a customer API.
    See : https://apidocs.chargebee.com/docs/api/cards#copy_card

---
+++ src/main/java/com/chargebee/models/ThirdPartyPaymentMethod.java
@@ -0,0 +1,46 @@
+package com.chargebee.models;
+
+import com.chargebee.*;
+import com.chargebee.internal.*;
+import com.chargebee.filters.*;
+import com.chargebee.filters.enums.SortOrder;
+import com.chargebee.internal.HttpUtil.Method;
+import com.chargebee.models.enums.*;
+import org.json.*;
+import java.io.*;
+import java.sql.Timestamp;
+import java.util.*;
+
+public class ThirdPartyPaymentMethod extends Resource<ThirdPartyPaymentMethod> {
+
+    //Constructors
+    //============
+
+    public ThirdPartyPaymentMethod(String jsonStr) {
+        super(jsonStr);
+    }
+
+    public ThirdPartyPaymentMethod(JSONObject jsonObj) {
+        super(jsonObj);
+    }
+
+    // Fields
+    //=======
+
+    public Type type() {
+        return reqEnum("type", Type.class);
+    }
+
+    public Gateway gateway() {
+        return reqEnum("gateway", Gateway.class);
+    }
+
+    public String referenceId() {
+        return optString("reference_id");
+    }
+
+    // Operations
+    //===========
+
+
+}
