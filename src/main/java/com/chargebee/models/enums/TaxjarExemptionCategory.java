57a35afcae29ef08975bbcab94e980c8c505b53f
cb-gaurav <gaurav@chargebee.com>
2020-01-08 07:25:39 +0000

    New attribute taxjar_product_code has been added in Addon and Plan resource.     * New input parameter taxjar_product_code has been added in create_an_addon, update_an_addon, create_a_plan and update_a_plan api     * New input parameter taxjar_exemption_category has been added in create_a_customer, update_a_customer , create_a_subscription and import_a_subscription api     * New input parameter no_expiry has been added in gift_subscription_estimate and create_a_gift api     * New attribute no_expiry has been added in Gift resource     * New endpoint update_gift has been added in Gift resource     * New value BUSINESS_CHECKING has been added in AccountType enum     * New value CCD has been added in EcheckType enum     * New event type GIFT_UPDATED has been added     * New enum taxjar_exemption_category has been added

--- src/main/java/com/chargebee/models/enums/TaxjarExemptionCategory.java
+++ src/main/java/com/chargebee/models/enums/TaxjarExemptionCategory.java
@@ -1,9 +1,9 @@
 package com.chargebee.models.enums;
 
-public enum AccountType {
-    CHECKING,
-    SAVINGS,
-    BUSINESS_CHECKING,
+public enum TaxjarExemptionCategory {
+    WHOLESALE,
+    GOVERNMENT,
+    OTHER,
     _UNKNOWN; /*Indicates unexpected value for this enum. You can get this when there is a
     java-client version incompatibility. We suggest you to upgrade to the latest version */
 }
