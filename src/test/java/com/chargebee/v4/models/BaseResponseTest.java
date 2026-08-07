5fdb8c83041c1934ae9349b3da892fb672604560
cb-karthikp <karthikp@chargebee.com>
2026-08-07 15:37:28 +0530

    Update BaseResponseTest.java

--- src/test/java/com/chargebee/v4/models/BaseResponseTest.java
+++ src/test/java/com/chargebee/v4/models/BaseResponseTest.java
@@ -12,14 +12,13 @@ import org.junit.jupiter.api.Test;
 class BaseResponseTest {
 
   @Test
-  @DisplayName("Should ignore null header keys when looking up by name")
-  void shouldIgnoreNullHeaderKeys() {
+  @DisplayName("Should look up headers case-insensitively")
+  void shouldLookUpHeadersCaseInsensitively() {
     HashMap<String, List<String>> headers = new HashMap<>();
-    headers.put(null, List.of("ignored"));
     headers.put("X-Chargebee-Telemetry", List.of("cb;time_ms=1"));
 
     BaseResponse response = new BaseResponse(new Response(200, headers, "{}".getBytes())) {};
 
-    assertEquals(List.of("cb;time_ms=1"), response.header("X-Chargebee-Telemetry"));
+    assertEquals(List.of("cb;time_ms=1"), response.header("x-chargebee-telemetry"));
   }
 }
